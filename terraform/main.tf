13a33db139f8c4edc0f2062a8b9f8640ba5cd797
Xavier Jeiziner <83689318+JEX-98@users.noreply.github.com>
2024-08-15 07:49:03 +0200

    added required tf and provider versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,17 @@
 // Bitnami (and MySQL) image.
 //***********************************************************
 
+terraform {
+  required_version = ">= 0.12"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
